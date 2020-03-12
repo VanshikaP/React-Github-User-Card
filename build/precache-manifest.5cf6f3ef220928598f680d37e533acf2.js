df13dd1c82645692491bbf2df0d889bf8097eb1b
VanshikaP <vanshikapundir92@gmail.com>
2020-03-12 01:16:05 -0700

    moving files

---
+++ build/precache-manifest.5cf6f3ef220928598f680d37e533acf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d6e8754715b42be7ff491382345b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ca5dfe8883f4da3211",
+    "url": "/static/css/main.c05d542c.chunk.css"
+  },
+  {
+    "revision": "4500d0f16c1a2dcf01d1",
+    "url": "/static/js/2.d6aab56d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.d6aab56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ca5dfe8883f4da3211",
+    "url": "/static/js/main.44abbbb3.chunk.js"
+  },
+  {
+    "revision": "141a08acdb2158060a04",
+    "url": "/static/js/runtime-main.dd2f6207.js"
+  }
+]);
